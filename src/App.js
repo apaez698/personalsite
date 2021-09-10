e24ca768c013168c196d4451173752af6c33b6d9
Sebastián Páez <sebastian.paez@rappi.com>
2021-09-10 08:07:15 -0500

    Test Change CI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          APAEZ site
+          Inicio del sitio
         </p>
         <a
           className="App-link"
