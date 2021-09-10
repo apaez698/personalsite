4e9b3b898a9fe4201db358ae24952a5820889041
Sebastián Páez <sebastian.paez@rappi.com>
2021-09-10 08:21:14 -0500

    Test PR

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Inicio del sitio
+          Test Pull
         </p>
         <a
           className="App-link"
